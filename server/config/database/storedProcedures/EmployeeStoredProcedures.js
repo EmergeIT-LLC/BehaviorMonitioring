4bf3914349d912fcac545479a5d248f1dc8392b2
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2024-07-15 23:04:35 -0400

    employee apis creation initiated
    employee verification, login, and profile api created

--- server/config/database/storedProcedures/EmployeeStoredProcedures.js
+++ server/config/database/storedProcedures/EmployeeStoredProcedures.js
@@ -1,4 +1,134 @@
 const db = require('../dbConnection');
 
+async function employeeExistByUsername(uName) {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM employee WHERE username = ?', [uName], (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(rows.length > 0); // Resolve with true if duplicate user found, false otherwise
+            }
+        });
+    });
+}
+
+async function employeeExistByID(uID) {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM employee WHERE employeeID = ?', [uID], (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(rows.length > 0); // Resolve with true if duplicate user found, false otherwise
+            }
+        });
+    });
+}
+
+async function employeeDataByUsername(uName) {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT employeeID, fName, lName, username, email, phone_number, role, account_status FROM employee WHERE username = ?', [uName], (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(rows[0]); // Resolve rows if user found
+            }
+        });
+    });
+}
+
+async function employeeDataById(uID) {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT employeeID, fName, lName, username, email, phone_number, role, account_status FROM employee WHERE employeeID = ?', [uID], (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(rows[0]); // Resolve rows if user found
+            }
+        });
+    });
+}
+
+async function employeePasswordByUsername(uName) {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT password FROM employee WHERE username = ?', [uName], (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(rows[0]); // Resolve rows if user found
+            }
+        });
+    });
+}
+
+async function employeePasswordById(uID) {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT password FROM employee WHERE employeeID = ?', [uID], (err, rows) => {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(rows[0]); // Resolve rows if user found
+            }
+        });
+    });
+}
+
+async function employeeUpdateEmployeeAccountByUsername(fName, lName, email, phone_number, pWord, uName) {
+    return new Promise((resolve, reject) => {
+        db.run('UPDATE Employee SET fName = ?, lName = ?, email = ?, phone_number = ?, password = ? WHERE username = ?', [fName, lName, email, phone_number, pWord, uName], function (err) {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(this.changes > 0); // Resolve with true if new user is added, false otherwise
+            }
+        });
+    });
+}
+
+async function employeeUpdateEmployeeAccountByID(fName, lName, email, phone_number, pWord, eID) {
+    return new Promise((resolve, reject) => {
+        db.run('UPDATE Employee SET fName = ?, lName = ?, email = ?, phone_number = ?, password = ? WHERE employeeID = ?', [fName, lName, email, phone_number, pWord, eID], function (err) {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(this.changes > 0); // Resolve with true if new user is added, false otherwise
+            }
+        });
+    });
+}
+
+async function employeeUpdateEmployeeAccountStatusByUsername(accountStatus, uName) {
+    return new Promise((resolve, reject) => {
+        db.run('UPDATE Employee SET account_status = ? WHERE username = ?', [accountStatus, uName], function (err) {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(this.changes > 0); // Resolve with true if new user is added, false otherwise
+            }
+        });
+    });
+}
+
+async function employeeUpdateEmployeeStatusAccountByID(accountStatus, eID) {
+    return new Promise((resolve, reject) => {
+        db.run('UPDATE Employee SET account_status = ? WHERE employeeID = ?', [accountStatus, eID], function (err) {
+            if (err) {
+                reject({ message: err.message });
+            } else {
+                resolve(this.changes > 0); // Resolve with true if new user is added, false otherwise
+            }
+        });
+    });
+}
+
 module.exports = {
+    employeeExistByUsername,
+    employeeExistByID,
+    employeeDataByUsername,
+    employeeDataById,
+    employeePasswordByUsername,
+    employeePasswordById,
+    employeeUpdateEmployeeAccountByUsername,
+    employeeUpdateEmployeeAccountByID,
+    employeeUpdateEmployeeAccountStatusByUsername,
+    employeeUpdateEmployeeStatusAccountByID
 }
--- server/routes/Employee.js
+++ server/routes/Employee.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 //Import Queries
+const employeeQueries = require('../config/database/storedProcedures/EmployeeStoredProcedures');
 const behaviorPlanExpirationCountDown = require('../functions/basic/behaviorPlanExpirationCountDown');
 const dateTimeFormat = require('../functions/basic/dateTimeFormat');
 const generateSecurityToken = require('../functions/basic/generateSecurityToken');
@@ -13,8 +14,109 @@ const saltRounds = 10;
 const { addYears } = require('date-fns');
 
 /*--------------------------------------------Authentication---------------------------------------------*/
+router.post('/validateEmployeeAccount', async (req, res) => {
+    try {
+        const uName = req.body.username;
+        
+        if (await employeeQueries.employeeExistByUsername(uName)) {
+            const employeeData = await employeeQueries.employeeDataByUsername(uName.toLowerCase());
+
+            if (employeeData.account_status === "In Verification") {
+                return res.json({ statusCode: 200, locatedAccount: true });
+            }
+            return res.json({ statusCode: 401, locatedAccount: false });            
+        }
+    } catch (error) {
+        return res.json({ statusCode: 500, serverMessage: 'A server error occurred', errorMessage: error.message });
+    }
+});
+
+router.post('/validateEmployeeAccount', async (req, res) => {
+    try {
+        const uName = req.body.username;
+        const password = req.body.password;
+    
+        if (await employeeQueries.employeeExistByUsername(uName)) {
+            const employeeData = await employeeQueries.employeeDataByUsername(uName.toLowerCase());
+
+            if (employeeData.account_status === "In Verification") {
+                bcrypt.hash(password, saltRounds, async function(err, hash) {
+                    if (await employeeQueries.employeeUpdateEmployeeAccountStatusByUsername("Active", uName))
+                    return res.json({ statusCode: 200, accountVerified: true });
+                });
+            }
+            return res.json({ statusCode: 401, locatedAccount: false });            
+        }
+    } catch (error) {
+        return res.json({ statusCode: 500, serverMessage: 'A server error occurred', errorMessage: error.message });
+    }
+});
+
+router.post('/verifyEmployeeLogin', async (req, res) => {
+    try {
+        const uName = req.body.username;
+        const password = req.body.password;
+
+        if (await employeeQueries.employeeExistByUsername(uName.toLowerCase())) {
+            const employeePassword = await employeeQueries.employeePasswordByUsername(uName.toLowerCase());
+
+            if (employeePassword.password.length > 0 || employeePassword.password !== null) {
+                const bcryptResult = await new Promise((resolve, reject) => {
+                    bcrypt.compare(password, employeePassword.password, (err, result) => {
+                        if (err){
+                            reject(err);
+                        }
+                        else {
+                            resolve(result);
+                        }
+                    });
+                });
+
+                if (bcryptResult) {
+                    const employeeData = await employeeQueries.employeeDataByUsername(uName.toLowerCase());
+                    
+                    if (employeeData.role === "root" || employeeData.role === "admin") {
+                        return res.json({ statusCode: 200, loginStatus: true, isAdmin: true });
+                    }
+                    else {
+                        return res.json({ statusCode: 200, loginStatus: true, isAdmin: false });
+                    }
+                }
+                else {
+                    return res.json({ statusCode: 403, serverMessage: 'Password is incorrect' });
+                }
+            }
+            else {
+                //Need to send the verification email to the employee and return 401 code if email is successfully sent...
+                return res.json({ statusCode: 401, serverMessage: 'User needs to authenticate their account' });
+            }
+        }
+        else {
+            return res.json({ statusCode: 401, serverMessage: 'Unauthorized user' });
+        }
+    } catch (error) {
+        return res.json({ statusCode: 500, serverMessage: 'A server error occurred', errorMessage: error.message });
+    }
+});
 
 /*-----------------------------------------------Employee-----------------------------------------------*/
+router.post('/getEmployeeData', async (req, res) => {
+    try {
+        const uName = req.body.username;
+
+        if (await employeeQueries.employeeExistByUsername(uName.toLowerCase())) {
+            const employeeData = await employeeQueries.employeeDataByUsername(uName.toLowerCase());
+            if (employeeData.length > 0) {
+                return res.json({ statusCode: 200, employeeData: employeeData });
+            }
+        }
+        else {
+            return res.json({ statusCode: 401, serverMessage: 'Unauthorized user' });
+        }
+    } catch (error) {
+        return res.json({ statusCode: 500, serverMessage: 'A server error occurred', errorMessage: error.message });
+    }
+});
 
 /*-------------------------------------------------ABA--------------------------------------------------*/
 
