be8f45be14b0fc58df178084e229f543ac85ab35
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2024-09-13 00:42:36 -0400

    Update targetbehavior.tsx

--- client/src/pages/targetbehavior.tsx
+++ client/src/pages/targetbehavior.tsx
@@ -5,7 +5,7 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Button from '../components/Button';
 import Loading from '../components/loading';
-import { GetLoggedInUserStatus, isCookieValid } from '../function/VerificationCheck';
+import { GetLoggedInUserStatus, GetLoggedInUser, isCookieValid } from '../function/VerificationCheck';
 import Axios from 'axios';
 
 const TargetBehavior: React.FC = () => {
@@ -16,8 +16,13 @@ const TargetBehavior: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const userLoggedIn = GetLoggedInUserStatus();
+    const loggedInUser = GetLoggedInUser();
     const cookieIsValid = isCookieValid();
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [statusMessage, setStatusMessage] = useState<React.ReactNode>('');
+    const [clientLists, setClientLists] = useState<{ value: string; label: string }[]>([]);
+    const [selectedClient, setSelectedClient] = useState<string>('');
+    const [selectedClientID, setSelectedClientID] = useState<number>(0);
 
     useEffect(() => {
         if (!userLoggedIn || !cookieIsValid) {
@@ -29,10 +34,54 @@ const TargetBehavior: React.FC = () => {
         }
         else {
             setIsLoading(true);
-            //Get target behaviors
+            getClientNames();
+            getClientTargetBehaviors();
         }
         setIsLoading(false);
     }, [userLoggedIn]);
+
+    const getClientNames = async () => {
+        const url = process.env.REACT_APP_Backend_URL + '/aba/getAllClientInfo';
+        try {
+            const response = await Axios.post(url, { "employeeUsername": loggedInUser });
+            if (response.data.statusCode === 200) {
+                setSelectedClient(response.data.clientData[0].fName + " " + response.data.clientData[0].lName);
+                setSelectedClientID(response.data.clientData[0].clientID);
+                const fetchedOptions = response.data.clientData.map((clientData: { clientID: number, fName: string, lName: string }) => ({
+                    value: clientData.clientID,
+                    label: `${clientData.fName} ${clientData.lName}`,
+                }));
+                setClientLists(fetchedOptions);
+            } else {
+                setStatusMessage(response.data.serverMessage);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const getClientTargetBehaviors = async () => {
+        if (selectedClientID === 0) return;
+
+        const url = process.env.REACT_APP_Backend_URL + '/aba/getClientTargetBehavior';
+        try {
+            const response = await Axios.post(url, {
+                "clientID": selectedClientID,
+                "employeeUsername": loggedInUser
+            });
+            if (response.data.statusCode === 200) {
+                const fetchedOptions = response.data.behaviorSkillData.map((behavior: { bsID: number, name: string, measurement: string }) => ({
+                    value: behavior.bsID,
+                    label: behavior.name,
+                    measurementType: behavior.measurement
+                }));
+            } else {
+                setStatusMessage(response.data.serverMessage);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
   return (
     <>
