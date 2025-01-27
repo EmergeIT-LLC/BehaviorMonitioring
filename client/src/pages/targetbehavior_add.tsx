4e26e12efa61d7c21c75c8d3819f11fbb96e0d40
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2025-01-27 07:12:54 -0500

    Update targetbehavior_add.tsx
    Add new behavior in progress

--- client/src/pages/targetbehavior_add.tsx
+++ client/src/pages/targetbehavior_add.tsx
@@ -5,8 +5,9 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Button from '../components/Button';
 import Loading from '../components/loading';
-import { GetLoggedInUserStatus, isCookieValid } from '../function/VerificationCheck';
+import { GetLoggedInUserStatus, GetLoggedInUser, isCookieValid } from '../function/VerificationCheck';
 import Axios from 'axios';
+import { get } from 'node:https';
 
 const AddTargetBehavior: React.FC = () => {
     useEffect(() => {
@@ -16,8 +17,15 @@ const AddTargetBehavior: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const userLoggedIn = GetLoggedInUserStatus();
+    const loggedInUser = GetLoggedInUser();
     const cookieIsValid = isCookieValid();
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [statusMessage, setStatusMessage] = useState<React.ReactNode>('');
+    const [clearMessageStatus, setClearMessageStatus] = useState<boolean>(false);
+    const [timerCount, setTimerCount] = useState<number>(0);
+    const [clientLists, setClientLists] = useState<{ value: string; label: string }[]>([]);
+    const [selectedClient, setSelectedClient] = useState<string>('');
+    const [selectedClientID, setSelectedClientID] = useState<number>(0);
 
     useEffect(() => {
         if (!userLoggedIn || !cookieIsValid) {
@@ -29,10 +37,44 @@ const AddTargetBehavior: React.FC = () => {
         }
         else {
             setIsLoading(true);
-            //Get target behaviors
+            getClientNames();
         }
         setIsLoading(false);
     }, [userLoggedIn]);
+
+    const getClientNames = async () => {
+        const url = process.env.REACT_APP_Backend_URL + '/aba/getAllClientInfo';
+        try {
+            const response = await Axios.post(url, { "employeeUsername": loggedInUser });
+            if (response.data.statusCode === 200) {
+                setSelectedClient(response.data.clientData[0].fName + " " + response.data.clientData[0].lName);
+                setSelectedClientID(response.data.clientData[0].clientID);
+                const fetchedOptions = response.data.clientData.map((clientData: { clientID: number, fName: string, lName: string }) => ({
+                    value: clientData.clientID,
+                    label: `${clientData.fName} ${clientData.lName}`,
+                }));
+                setClientLists(fetchedOptions);
+            } else {
+                setStatusMessage(response.data.serverMessage);
+            }
+            setIsLoading(false);
+        } catch (error) {
+            console.error(error);
+            setIsLoading(false);
+        }
+    };
+
+        useEffect(() => {
+            if (timerCount > 0) {
+                const timer = setTimeout(() => setTimerCount(timerCount - 1), 1000);
+                return () => clearTimeout(timer);
+            }
+            if (timerCount === 0 && clearMessageStatus) {
+                setClearMessageStatus(false);
+                setStatusMessage('')
+            }
+        }, [timerCount, clearMessageStatus]);
+    
 
   return (
     <>
