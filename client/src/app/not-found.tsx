b7f2124da07af081c4f511e6f1001433a64587e7
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2025-12-22 21:22:19 -0500

    Add custom 404 NotFound page component
    Introduces a new NotFound React component for handling 404 pages, including branding, navigation back to the homepage, and consistent header/footer layout.

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,23 +1,20 @@
 "use client";
 import React from 'react';
-import Head from 'next/head';
-import componentStyles from '../../styles/components.module.scss';
-import Header from '../../components/header';
-import Footer from '../../components/footer';
-import Link from '../../components/Link';
-import Logo from '../BMetrics-logo-removebg.png'
+import Image from 'next/image';
+import componentStyles from '../styles/components.module.scss';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Link from '../components/Link';
+import Logo from './BMetrics-logo-removebg.png'
 
-const Pagenotfound: React.FC = () => {
+function NotFound() {
     return (
         <>
             <Header/>
-            <Head>
-                <title>404 Page Not Found - BMetrics</title>
-            </Head>
             <div className={componentStyles.pageBody}>
                 <main>
                     <div className={componentStyles.pageNotFoundPageBody}>
-                        <img src={Logo.src} alt ="Behavior Monitoring Logo" />
+                        <Image src={Logo} alt="Behavior Monitoring Logo" />
                         <p>The page you are looking for does not exist. <br/> Please return to the <Link href="/" hrefType="link" placeholder="homepage"/> for your convenience.</p>
                     </div>
                 </main>
@@ -27,4 +24,4 @@ const Pagenotfound: React.FC = () => {
     );
 }
 
-export default Pagenotfound;
+export default NotFound;
